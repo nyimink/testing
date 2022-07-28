0bc593ffd1c5b793895375cfe1ef7b831eab64c9
Nyi Min Khant <nyiminkx01@gmail.com>
2022-07-28 15:57:01 +0630

    testtest

---
+++ js.js
@@ -0,0 +1,7 @@
+var btn = document.querySelector("#btn1");
+
+function myFunction() {
+     document.getElementById("cat").innerHTML = "<i>hello</i>";
+}
+
+btn.addEventListener("click", myFunction);
